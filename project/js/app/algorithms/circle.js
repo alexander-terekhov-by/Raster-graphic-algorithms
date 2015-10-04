cb2a747f49de571e0adae2c7d42d35b80ac25355
teranik1994 <TerAN_1994>
2015-10-04 14:21:31 +0300

    Circle algorithm

--- project/js/app/algorithms/circle.js
+++ project/js/app/algorithms/circle.js
@@ -1,10 +1,10 @@
-
 function algorithmCircle(p1, p2) {
     var lengthX = Math.abs(p2.x - p1.x);
     var lengthY = Math.abs(p2.y - p1.y);
     var radius = Math.round(Math.sqrt(Math.pow(lengthX, 2) + Math.pow(lengthY, 2)));
+    $console.append("radius " + radius);
 
-    var x = 0, y = radius, gap = 0, delta = (2 - 2 * radius);
+    var x = 0, y = radius, gap = 0, delta = 1 - 2 * radius;
 
 
     $("#next-step").click(function () {
@@ -14,27 +14,23 @@ function algorithmCircle(p1, p2) {
         }
     });
 
-
     function stepFunction() {
         drawPixel(p1.x + x, p1.y + y);
         drawPixel(p1.x + x, p1.y - y);
         drawPixel(p1.x - x, p1.y - y);
         drawPixel(p1.x - x, p1.y + y);
         gap = 2 * (delta + y) - 1;
-        if (delta < 0 && gap <= 0) {
-            x++;
-            delta += 2 * x + 1;
-
-        } else if (delta > 0 && gap > 0) {
-            y--;
-            delta -= 2 * y + 1;
-
-
-        } else {
-
-            x++;
-            delta += 2 * (x - y);
-            y--;
+        if ((delta < 0) && (gap < 0)) {
+            delta += 2 * ++x + 1;
+            return;
         }
+        gap = 2 * (delta - x) - 1;
+        if ((delta > 0) && (gap > 0)) {
+            delta += 1 - 2 * --y;
+            return;
+        }
+        x++;
+        y--;
+        delta -= 2 * ( y - x - 1);
     }
 }
